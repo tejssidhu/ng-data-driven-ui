345effb58d3904027c279c92445fd8d5e3dcaa6f
Tej Sidhu <tejssidhu@gmail.com>
2018-04-02 20:35:25 +0100

    toggle background colour in content block 1

--- src/app/content-blocks/content-block1.component.ts
+++ src/app/content-blocks/content-block1.component.ts
@@ -3,19 +3,27 @@ import { Component, Injector } from '@angular/core';
 @Component({
 	moduleId: module.id,
 	selector: 'app-content-block-1',
+	styles: [
+		'div { color: white; }'
+	],
 	templateUrl: 'content-block1.component.html'
 })
 export class ContentBlock1Component {
 	message = '';
 	backgroundColour = '';
+	oldBackgroundColor = '';
 
 	constructor(injector: Injector) {
 		this.message = injector.get('message');
 		this.backgroundColour = injector.get('background-color');
+		this.oldBackgroundColor = 'blue';
 	}
 
-	changeBackgroundColour(event) {
-		this.backgroundColour = 'blue';
+	toggleBackgroundColour(event) {
+		const currentBackgroundColour = this.backgroundColour;
+		this.backgroundColour = this.oldBackgroundColor;
+		this.oldBackgroundColor = currentBackgroundColour;
+
 		return false;
 	}
 }
